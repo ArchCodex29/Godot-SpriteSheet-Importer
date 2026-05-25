3fb6d8e7e22e534f7f757f019e150855c7524664
Pedro Oliveira <pncolvr@gmail.com>
2026-05-25 22:14:23 +0100

    fix: importer not showing

--- addons/sprite_sheet_importer/sprite_sheet_importer.gd
+++ addons/sprite_sheet_importer/sprite_sheet_importer.gd
@@ -1,25 +1,22 @@
 @tool
 extends EditorPlugin
 
-var inspector_plugin: EditorImportPlugin = null
 const Importer = preload("res://addons/sprite_sheet_importer/sample_importer.gd")
+var inspector_plugin: EditorImportPlugin = Importer.new()
+
 func _enable_plugin() -> void:
 	# Add autoloads here.
-	#inspector_plugin = MyCustomImporter.new()
-	inspector_plugin = Importer.new()
-	add_import_plugin(inspector_plugin)
+	pass
 
 
 func _disable_plugin() -> void:
 	# Remove autoloads here.
-	#inspector_plugin = null
-	remove_import_plugin(inspector_plugin)
 	pass
-
+	
 func _enter_tree():
-	#add_import_plugin(inspector_plugin, true)
+	add_import_plugin(inspector_plugin, true)
 	pass
 
 func _exit_tree():
-	#remove_import_plugin(inspector_plugin)
+	remove_import_plugin(inspector_plugin)
 	pass
